5ceb0e9c7dc7dc674dceb8b003c5605d3114ce86
Shawn Azdam <shawn@azdam.com>
2023-01-21 10:27:59 -0500

    Refactor ExoPlayer

---
+++ TMessagesProj/src/main/java/com/google/android/exoplayer2/source/EmptySampleStream.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.android.exoplayer2.source;
+
+<<<<<<< HEAD:TMessagesProj/src/main/java/org/telegram/messenger/exoplayer2/source/EmptySampleStream.java
+import org.telegram.messenger.exoplayer2.C;
+import org.telegram.messenger.exoplayer2.FormatHolder;
+import org.telegram.messenger.exoplayer2.decoder.DecoderInputBuffer;
+
+import java.io.IOException;
+=======
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.FormatHolder;
+import com.google.android.exoplayer2.decoder.DecoderInputBuffer;
+>>>>>>> upstream/master:TMessagesProj/src/main/java/com/google/android/exoplayer2/source/EmptySampleStream.java
+
+/** An empty {@link SampleStream}. */
+public final class EmptySampleStream implements SampleStream {
+
+  @Override
+  public boolean isReady() {
+    return true;
+  }
+
+  @Override
+  public void maybeThrowError() {
+    // Do nothing.
+  }
+
+  @Override
+  public int readData(
+      FormatHolder formatHolder, DecoderInputBuffer buffer, @ReadFlags int readFlags) {
+    buffer.setFlags(C.BUFFER_FLAG_END_OF_STREAM);
+    return C.RESULT_BUFFER_READ;
+  }
+
+  @Override
+  public int skipData(long positionUs) {
+    return 0;
+  }
+}
