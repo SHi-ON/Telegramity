ebfe2c75d1c8d4750ca8150e13a551f5521bfb14
SHi-ON <10985542+SHi-ON@users.noreply.github.com>
2023-01-26 15:59:15 -0500

    Add PhoneFormat changes

--- TMessagesProj/src/main/java/org/telegram/PhoneFormat/PhoneFormat.java
+++ TMessagesProj/src/main/java/org/telegram/PhoneFormat/PhoneFormat.java
@@ -24,6 +24,8 @@
 
 package org.telegram.PhoneFormat;
 
+import androidx.annotation.NonNull;
+
 import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.FileLog;
 
@@ -181,6 +183,7 @@ public class PhoneFormat {
         return res;
     }
 
+    @NonNull
     public String format(String orig) {
         if (!initialzed) {
             return orig;
@@ -193,7 +196,7 @@ public class PhoneFormat {
                 CallingCodeInfo info = findCallingCodeInfo(rest);
                 if (info != null) {
                     String phone = info.format(rest);
-                    return "+" +  phone;
+                    return "+" + phone;
                 } else {
                     return orig;
                 }
