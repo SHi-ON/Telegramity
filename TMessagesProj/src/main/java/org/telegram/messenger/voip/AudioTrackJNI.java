eb29b0fc485bec4d892e6b883f6af65dfaa073cc
SHi-ON <10985542+SHi-ON@users.noreply.github.com>
2023-02-23 16:56:59 -0500

    Resolve voip module changes

--- TMessagesProj/src/main/java/org/telegram/messenger/voip/AudioTrackJNI.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/voip/AudioTrackJNI.java
@@ -3,9 +3,6 @@ package org.telegram.messenger.voip;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.util.Log;
-
-import org.telegram.messenger.FileLog;
 
 import java.nio.ByteBuffer;
 
@@ -13,17 +10,17 @@ import java.nio.ByteBuffer;
  * Created by grishka on 20.12.16.
  */
 
-public class AudioTrackJNI{
+public class AudioTrackJNI {
 	private AudioTrack audioTrack;
-	private byte[] buffer=new byte[960*2];
+	private byte[] buffer = new byte[960 * 2];
 	private boolean running;
 	private Thread thread;
-	private int bufferSize;
-	private long nativeInst;
 	private boolean needResampling;
 
-	public AudioTrackJNI(long nativeInst) {
-		this.nativeInst = nativeInst;
+	private long nativeInst;
+
+	public AudioTrackJNI(long ptr) {
+		nativeInst = ptr;
 	}
 
 	private int getBufferSize(int min, int sampleRate) {
@@ -35,37 +32,43 @@ public class AudioTrackJNI{
 			throw new IllegalStateException("already inited");
 		}
 		int size = getBufferSize(bufferSize, 48000);
-		this.bufferSize = bufferSize;
-		audioTrack=new AudioTrack(AudioManager.STREAM_VOICE_CALL, 48000, channels==1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, size, AudioTrack.MODE_STREAM);
-		if(audioTrack.getState()!=AudioTrack.STATE_INITIALIZED){
-			try{
+		audioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, 48000, channels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, size, AudioTrack.MODE_STREAM);
+		if (audioTrack.getState() != AudioTrack.STATE_INITIALIZED) {
+			VLog.w("Error initializing AudioTrack with 48k, trying 44.1k with resampling");
+			try {
 				audioTrack.release();
-			}catch(Throwable x){}
-			size=getBufferSize(bufferSize*6, 44100);
-			FileLog.d("buffer size: "+size);
-			audioTrack=new AudioTrack(AudioManager.STREAM_VOICE_CALL, 44100, channels==1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, size, AudioTrack.MODE_STREAM);
-			needResampling=true;
+			} catch (Throwable ignore) {
+			}
+			size = getBufferSize(bufferSize * 6, 44100);
+			VLog.d("buffer size: " + size);
+			audioTrack = new AudioTrack(AudioManager.STREAM_VOICE_CALL, 44100, channels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT, size, AudioTrack.MODE_STREAM);
+			needResampling = true;
 		}
 	}
 
 	public void stop() {
-		if(audioTrack!=null)
-			audioTrack.stop();
+		if (audioTrack != null) {
+			try {
+				audioTrack.stop();
+			} catch (Exception ignore) {
+
+			}
+		}
 	}
 
 	public void release() {
 		running = false;
-		if(thread!=null){
-			try{
+		if (thread != null) {
+			try {
 				thread.join();
-			}catch(InterruptedException e){
-				FileLog.e(e);
+			} catch (InterruptedException e) {
+				VLog.e(e);
 			}
-			thread=null;
+			thread = null;
 		}
-		if(audioTrack!=null){
+		if (audioTrack != null) {
 			audioTrack.release();
-			audioTrack=null;
+			audioTrack = null;
 		}
 	}
 
@@ -82,41 +85,38 @@ public class AudioTrackJNI{
 			throw new IllegalStateException("thread already started");
 		}
 		running = true;
-		thread = new Thread(new Runnable() {
-			@Override
-			public void run() {
-				try{
-					audioTrack.play();
-				}catch(Exception x){
-					FileLog.e("error starting AudioTrack", x);
-					return;
-				}
-				ByteBuffer tmp48=needResampling ? ByteBuffer.allocateDirect(960*2) : null;
-				ByteBuffer tmp44=needResampling ? ByteBuffer.allocateDirect(882*2) : null;
-				while (running) {
-					try {
-						if(needResampling){
-							nativeCallback(buffer);
-							tmp48.rewind();
-							tmp48.put(buffer);
-							Resampler.convert48to44(tmp48, tmp44);
-							tmp44.rewind();
-							tmp44.get(buffer, 0, 882*2);
-							audioTrack.write(buffer, 0, 882*2);
-						}else{
-							nativeCallback(buffer);
-							audioTrack.write(buffer, 0, 960*2);
-						}
-						if (!running) {
-							audioTrack.stop();
-							break;
-						}
-					} catch (Exception e) {
-						FileLog.e(e);
-					}
-				}
-				Log.i("tg-voip", "audiotrack thread exits");
+		thread = new Thread(() -> {
+			try {
+				audioTrack.play();
+			} catch (Exception x) {
+				VLog.e("error starting AudioTrack", x);
+				return;
 			}
+			ByteBuffer tmp48 = needResampling ? ByteBuffer.allocateDirect(960 * 2) : null;
+			ByteBuffer tmp44 = needResampling ? ByteBuffer.allocateDirect(882 * 2) : null;
+			while (running) {
+				try {
+					if (needResampling) {
+						nativeCallback(buffer);
+						tmp48.rewind();
+						tmp48.put(buffer);
+						Resampler.convert48to44(tmp48, tmp44);
+						tmp44.rewind();
+						tmp44.get(buffer, 0, 882 * 2);
+						audioTrack.write(buffer, 0, 882 * 2);
+					} else {
+						nativeCallback(buffer);
+						audioTrack.write(buffer, 0, 960 * 2);
+					}
+					if (!running) {
+						audioTrack.stop();
+						break;
+					}
+				} catch (Exception e) {
+					VLog.e(e);
+				}
+			}
+			VLog.i("audiotrack thread exits");
 		});
 		thread.start();
 	}
