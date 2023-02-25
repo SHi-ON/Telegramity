f4684685a3d0c6e1609d03090db9196f67c2194b
SHi-ON <10985542+SHi-ON@users.noreply.github.com>
2023-02-25 06:10:38 -0500

    Changes to be committed:
    	modified:   TMessagesProj/src/main/java/org/telegram/messenger/AppStartReceiver.java
    	new file:   TMessagesProj/src/main/java/org/telegram/messenger/AuthTokensHelper.java
    	modified:   TMessagesProj/src/main/java/org/telegram/messenger/AuthenticatorService.java

--- TMessagesProj/src/main/java/org/telegram/messenger/AuthenticatorService.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/AuthenticatorService.java
@@ -1,9 +1,9 @@
 /*
- * This is the source code of Telegram for Android v. 3.x.x.
+ * This is the source code of Telegram for Android v. 5.x.x.
  * It is licensed under GNU GPL v. 2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
  *
- * Copyright Nikolai Kudashov, 2013-2017.
+ * Copyright Nikolai Kudashov, 2013-2018.
  */
 
 package org.telegram.messenger;
@@ -22,11 +22,9 @@ import android.os.IBinder;
 public class AuthenticatorService extends Service {
 
     private static class Authenticator extends AbstractAccountAuthenticator {
-        private final Context context;
 
         public Authenticator(Context context) {
             super(context);
-            this.context = context;
         }
 
         @Override
