13e6011150415f2c3faedaf9e0ef5725773c2f02
SHi-ON <10985542+SHi-ON@users.noreply.github.com>
2023-03-19 20:06:59 -0600

    Changes to be committed:
    	modified:   TMessagesProj/src/main/java/org/telegram/messenger/OpenChatReceiver.java
    	modified:   TMessagesProj/src/main/java/org/telegram/messenger/PopupReplyReceiver.java

--- TMessagesProj/src/main/java/org/telegram/messenger/OpenChatReceiver.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/OpenChatReceiver.java
@@ -1,9 +1,9 @@
 /*
- * This is the source code of Telegram for Android v. 3.x.x.
+ * This is the source code of Telegram for Android v. 5.x.x.
  * It is licensed under GNU GPL v. 2 or later.
  * You should have received a copy of the license in this archive (see LICENSE).
  *
- * Copyright Nikolai Kudashov, 2013-2017.
+ * Copyright Nikolai Kudashov, 2013-2018.
  */
 
 package org.telegram.messenger;
@@ -22,9 +22,21 @@ public class OpenChatReceiver extends Activity {
         Intent intent = getIntent();
         if (intent == null) {
             finish();
+            return;
         }
         if (intent.getAction() == null || !intent.getAction().startsWith("com.tmessages.openchat")) {
             finish();
+            return;
+        }
+        try {
+            long chatId = intent.getLongExtra("chatId", intent.getIntExtra("chatId", 0));
+            long userId = intent.getLongExtra("userId", intent.getIntExtra("userId", 0));
+            int encId = intent.getIntExtra("encId", 0);
+            if (chatId == 0 && userId == 0 && encId == 0) {
+                return;
+            }
+        } catch (Throwable e) {
+            FileLog.e(e);
             return;
         }
         Intent intent2 = new Intent(this, LaunchActivity.class);
@@ -32,5 +44,9 @@ public class OpenChatReceiver extends Activity {
         intent2.putExtras(intent);
         startActivity(intent2);
         finish();
+
+        /*
+
+         */
     }
 }
